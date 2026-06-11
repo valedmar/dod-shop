7bd97c4bb5537b5c2db07b60eb83cfed6e513bb8
valedmar <valdemarberglauritsen@gmail.com>
2026-06-11 10:21:09 +0200

    added .env and .gitignore

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-echo 'hello world';
+echo "hello world", "<br>\n";
 
+
+$appName = $_ENV['APP_NAME'];
+
+echo $appName;
 
 ?>
